8fe7e37a5fac0314347eadf14fa491e19e614fc8
TsionZerihun <tsionzelekezerihun@gmail.com>
2022-07-04 21:59:01 +0300

    edding | statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 | idx > len(my_list) - 1:
+    n = len(my_list)
+    if idx < 0:
+        return None
+    elif idx > n - 1:
         return None
     else:
         return my_list[idx]
