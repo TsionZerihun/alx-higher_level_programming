d533e1d82dca2d1f74ab3dd78920e538856116a1
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-10-10 18:08:33 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = 0
     try:
-        div = a / b
-        return div
-    except:
-        div = None
-        return None
+        n = a / b
+    except (ZeroDivisionError):
+        n = None
     finally:
-        print("Inside result: {}".format(div))
+        print("Inside result: {}".format(n))
+    return n
