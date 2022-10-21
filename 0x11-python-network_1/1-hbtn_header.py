be77f0214b240ff3a531d11073a79fc58178118d
TsionZerihun <tsionzelekezerihun@gmail.com>
2022-10-21 22:40:28 +0300

    making files execuable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""
-takes in a URL, sends a request to the URL and displays the value of the
-X-Request-Id variable found in the header of the response
-"""
+"""Fetches header"""
+import urllib.request
+import sys
+
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])
+
 if __name__ == "__main__":
-    import urllib.request as request
-    from sys import argv
-    req = request.Request(argv[1])
-    with request.urlopen(req) as r:
-        print(r.headers.get('X-Request-Id'))
+    fetcher()
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
-"""
+"""Send POST"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+def sender():
+    """sender"""
+    v = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(v)
+    data = data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
+
 if __name__ == "__main__":
-    import urllib.parse as parse
-    import urllib.request as request
-    from sys import argv
-    values = {'email': argv[2]}
-    data = parse.urlencode(values).encode('utf-8')
-    req = request.Request(argv[1], data)
-    with request.urlopen(req) as r:
-        print(r.read().decode('utf-8'))
+    sender()
