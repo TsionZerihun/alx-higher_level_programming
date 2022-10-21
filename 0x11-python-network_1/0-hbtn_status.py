849e5eedf7860bea388e8cd12f2b1788bcef31f0
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-10-21 17:25:18 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python3
-# Script that fetches https://alx-intranet.hbtn.io/status
-
-import urllib.request
-url =  "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(url) as r:
-     data = r.read()
-     data = data.decode("UTF-8")
-     print(data)
-
-
+from urllib import request
+url = "https://alx-intranet.hbtn.io/status"
+with request.urlopen(url) as r:
+    data = r.read()
+    print("Body respone:")
+    print("\t - type: {} ".format(type(data)))
+    print("\t - content: {} ".format(data))
+    print("\t - utf8 content: {} ".format(data.decode("UTF-8")))
