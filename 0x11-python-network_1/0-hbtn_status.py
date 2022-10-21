1f95207643a83d3fcac6125f99eb8e8941d94bc4
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-10-21 17:58:10 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ ==  "__main__":
         print('Body respone:')
         print("\t - type: {}".format(type(data)))
         print("\t - content: {}".format(data))
-        print("\t - utf8 content: {}".format(data.decode("UTF-8")))
+        print("\t - utf8 content: {}".format(data.decode('UTF-8')))
