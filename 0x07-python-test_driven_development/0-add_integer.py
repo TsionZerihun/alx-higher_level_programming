59a9e51ee07aed7f9144544b1ee9011daca41a7e
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-12-12 11:26:18 +0300

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,13 +8,10 @@ def add_integer(a, b=98):
     """
         Adds two integers
     """
-    try:
-        assert type(a) in (float, int)
-    except:
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    try:
-        assert type(b) in (float, int)
-    except:
+        
+    if type(b) != int and typeba) != float:
         raise TypeError("b must be an integer")
     if type(a) == float:
         a = int(a)
