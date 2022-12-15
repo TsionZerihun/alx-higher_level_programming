b400261d943c2ebd8e2cb9012accdca25ba4941a
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-12-15 09:42:15 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -22,8 +22,8 @@ def load_from_json_file(filename):
 filename = "add_item.json"
 json_list = []
 
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
+#if os.path.exists(filename):
+json_list = load_from_json_file(filename)
 
 for i in range(1, len(sys.argv)):
     json_list.append(sys.argv[i])
