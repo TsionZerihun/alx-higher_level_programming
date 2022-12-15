57abc0d7894ef0daa80dcb12d03e59d64ecb8032
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-12-15 11:26:45 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""Load, add, save"""
-
-
+from os import path
 from sys import argv
-from os.path import exists
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
-namefile = "add_item.json"
-argc = len(argv)
-
-new_list = []
-
-if exists(namefile):
-    new_list = load_from_json_file(namefile)
-
-if (argc == 1):
-    save_to_json_file([], namefile)
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
 else:
-    for i in range(1, argc):
-        new_list.append(argv[i])
-    save_to_json_file(new_list, namefile)
+    obj_json_file = []
+
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
+
+save_to_json_file(obj_json_file, 'add_item.json')
