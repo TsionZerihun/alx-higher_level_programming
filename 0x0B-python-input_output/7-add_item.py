a6be4e1d96211434a99854dce743cb9a0fbffed5
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-12-15 09:33:26 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-""" Load, add, save  """
-from sys import argv
+"""
+A script that adds all arguments to a Python list,
+and then save them to a file
+"""
 
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
+import os.path
+import json
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
 
-""" Script that adds all arguments to a Python list,
-and then save them to a file """
-try:
-    add_i = load_from_json_file('add_item.json')
-    save_to_json_file(add_i + argv[1:], 'add_item.json')
-except Exception:
-    save_to_json_file(argv[1:], 'add_item.json')
+filename = "add_item.json"
+json_list = []
+
+if os.path.exists(filename):
+    json_list = load(filename)
+
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save(json_list, filename)
