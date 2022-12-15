5a29f289940302b7d00592061458d8868b358287
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2022-12-15 09:41:12 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,16 +8,24 @@ and then save them to a file
 import sys
 import os.path
 import json
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
-
+#save = __import__('5-save_to_json_file').save_to_json_file
+def save_to_json_file(my_obj, filename):
+    """ Function that writes an Object to a text file,
+    using a JSON representation """
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
+#load = __import__('6-load_from_json_file').load_from_json_file
+def load_from_json_file(filename):
+    """ Function that creates an Object from a “JSON file” """
+    with open(filename, 'r') as f:
+        return (json.load(f))
 filename = "add_item.json"
 json_list = []
 
 if os.path.exists(filename):
-    json_list = load(filename)
+    json_list = load_from_json_file(filename)
 
 for i in range(1, len(sys.argv)):
     json_list.append(sys.argv[i])
 
-save(json_list, filename)
+save_to_json_file(json_list, filename)
