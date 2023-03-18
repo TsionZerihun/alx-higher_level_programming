7697dab37748dce8116a074fdc5208fcc4c2e0de
Tsion <101357449+TsionZerihun@users.noreply.github.com>
2023-03-18 19:17:47 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,12 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost';
+-- Create database 'hbtn_0d_2'
+-- If database already exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user 'user_0d_2'
+-- User password should be set to 'user_0d_2_pwd'
+-- If user already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- User 'user_0d_2' should have only SELECT privilege in db 'hbtn_0d_2'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
